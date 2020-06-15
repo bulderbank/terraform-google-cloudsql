cba06f774a61c0b9d704cff7b0f5a44d0983c7c6
Hans Kristian Flaatten <hans.flaatten@bulderbank.no>
2020-06-15 23:55:59 +0200

    terraform: add even more outputs (#4)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,26 @@
-output "databases" {
-  value = google_sql_database.database.*.name
+output "instance" {
+  value = google_sql_database_instance.instance.connection_name
+}
+
+output "address" {
+  value     = google_sql_database_instance.instance.ip_address.0.ip_address
+  sensitive = true
 }
 
 output "secrets" {
   value = kubernetes_secret.credential_secret.*.metadata.0.name
 }
 
-output "address" {
-  value = google_sql_database_instance.instance.ip_address.0.ip_address
+output "databases" {
+  value = google_sql_database.database.*.name
+}
+
+output "usernames" {
+  value     = google_sql_user.database_user.*.name
+  sensitive = true
+}
+
+output "passwords" {
+  value     = google_sql_user.database_user.*.password
+  sensitive = true
 }
